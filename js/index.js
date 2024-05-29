da4f3cddb4d0a4e10d4d8061aa19ab6136b41a0c
oyatillo0509 <tilloxon0509@gmail.com>
2024-05-29 12:53:19 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,124 @@
+// N-1
+// let a = +prompt("son kriting");
+// if (a >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-2
+// let a = +prompt("son kriting");
+// if (a % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-3
+// let a = +prompt("son kiriting");
+// if (a%2==0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-4
+// let a=+prompt("1-sonni kiriting");
+// let b=+prompt("2-sonni kiriting");
+// if(a>2 && b<=3){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// N-5
+// let num_1= +prompt("1-sonni kiriting");
+// let num_2 = +prompt("2-sonni kiriting");
+// if (num_1 >= 0 || num_2 < -2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-6
+// let num_1 = +prompt("1-sonni kiriting");
+// let num_2 = +prompt("2-sonni kiriting");
+// let num_3 = +prompt("3-sonni kiriting");
+// if (num_1<=num_2 && B<=num_3){
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-7
+// let num_1 = +prompt("1-sonni kiriting");
+// let num_2= +prompt("2-sonni kiriting");
+// let num_2 = +prompt("3-sonni kiriting");
+// if (num_1<num_2 && num_2<num_3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-8
+// let num_1 = +prompt("1-sonni kiriting");
+// let num_2= +prompt("2-sonni kiriting");
+// if (num_1 % 2 == 1 && num_2 % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+
+// N-9
+// let num_1 = +prompt("1-sonni kiriting");
+// let num_2 = +prompt("2-sonni kiriting");
+// if (num_1 % 2 == 1 || num_2 % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+
+// N - 10
+// chiqmadi
+
+// N-11
+
+// let son_1 = Number(prompt("1-sonni kiriting"));
+// let son_2 = Number(prompt("2-sonni kiriting"));
+
+// let toq = son_1 % 2 === 1 && son_2 % 2 === 1;
+// let juft = son_2 % 2 === 0 && son_2 % 2 === 0;
+
+// if (toq || juft) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-12
+// let num_1 = +prompt("A sonni kiriting");
+// let num_2 = +prompt("B sonni kiriting");
+// let num_3 = +prompt("C sonni kiriting");
+
+// if (num_1 >= 0 && num_2 >= 0 && num_3 >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-13
+// let num_1 = +prompt("A sonni kiriting");
+// let num_2 = +prompt("B sonni kiriting");
+// let num_3 = +prompt("C sonni kiriting");
+
+// if (num_1 >= 0 || num_2 >= 0 || num_3 >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// N-14
+xxxxxxx
+
+// N-15
+xxxxxxxx
+
